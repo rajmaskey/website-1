188bab43ea7a287784dfc0841c505256b98473ae
UNITY_ <rajmaskey017@gmail.com>
2022-04-19 16:08:30 +0545

    Bug fixes
    bug fixes and changed little

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.get('./index.html');
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
